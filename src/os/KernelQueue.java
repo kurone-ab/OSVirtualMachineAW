f4dae7ffb3ed3681b79590c32d0e808de2767919
ygnaiih1680 <48740869+ygnaiih1680@users.noreply.github.com>
2019-10-19 00:14:32 +0900

    Create KernelQueue.java
    add

---
+++ src/os/KernelQueue.java
@@ -0,0 +1,9 @@
+package os;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class KernelQueue extends LinkedBlockingQueue<ProcessAW> {
+	private static final KernelQueue queue = new KernelQueue();
+
+	public static void 
+}
