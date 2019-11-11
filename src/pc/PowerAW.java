a388fbe625787a1971345701a6cecd544d649b3c
Sungje Park <tjdwp1999@naver.com>
2019-11-11 20:41:24 +0900

    edit parser with regex

--- src/pc/PowerAW.java
+++ src/pc/PowerAW.java
@@ -6,21 +6,25 @@ import pc.mainboard.MainBoard;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Vector;
 
 public class PowerAW {
     public static void main(String[] args) {
 //		MainBoard mainBoard = new MainBoard();
 //		mainBoard.on();
-        StringBuilder builder = new StringBuilder();
-        try {
-            Scanner scanner = new Scanner(new File("exe/test.exw"));
-            while (scanner.hasNextLine())
-                builder.append(scanner.nextLine()).append("\r\n");
-			ParserAW.prepareParsing(builder.toString());
-			ParserAW.parse();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        int i = 0;
+        int[] array = new int[5];
+        Vector<Integer> vector = new Vector<>();
+        vector.add(i++);
+        vector.add(i++);
+        vector.add(i++);
+        vector.add(i++);
+        vector.add(i++);
+        i = 0;
+        for (int a : vector)
+            array[i++] = a;
+        System.out.println(Arrays.toString(array));
     }
 }
